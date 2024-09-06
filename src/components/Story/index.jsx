837844907890ea499c4abcca44300ac53e6c570d
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-09-06 08:50:26 +0600

    update2

--- src/components/Story/index.jsx
+++ src/components/Story/index.jsx
@@ -9,7 +9,7 @@ const Story = () => {
     <section className="py-28 relative bg-lightCream z-0">
       <div className="container mx-auto">
         {/* story top */}
-        <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-row items-center justify-between text-black">
           {storyList.map((story) => (
             <Card
               Key={story.id}
@@ -33,10 +33,10 @@ const Story = () => {
           <div className="w-5/12">
             {/* top */}
             <div className="">
-              <h1 className="text-2xl font-extrabold font-Cormorant pb-3">
+              <h1 className="text-2xl font-extrabold font-Cormorant pb-3 text-black">
                 The Story
               </h1>
-              <p className="font-josefin text-sm">
+              <p className="font-josefin text-sm text-[#555555]">
                 Lorem Ipsum is that it has a more-or-less normal distribution of
                 letters, as opposed to using 'Content here, content gfshere',
                 makinlook like readable English. Many desktop publishing
@@ -47,15 +47,19 @@ const Story = () => {
             <div className="flex flex-row pt-5">
               {/* left */}
               <div className="">
-                <h1 className="text-xl font-bold font-Cormorant">1996</h1>
-                <p className="font-josefin text-sm mt-1">
+                <h1 className="text-xl font-bold font-Cormorant text-black">
+                  1996
+                </h1>
+                <p className="font-josefin text-sm mt-1 text-[#555555]">
                   Lorem Ipsum is that it has a more-or-less normal distribution
                 </p>
               </div>
               {/* right */}
               <div className="">
-                <h1 className="text-xl font-bold font-Cormorant">2021</h1>
-                <p className="font-josefin text-sm mt-1">
+                <h1 className="text-xl font-bold font-Cormorant text-black">
+                  2021
+                </h1>
+                <p className="font-josefin text-sm mt-1 text-[#555555]">
                   Lorem Ipsum is that it has a more-or-less normal Content
                   content
                 </p>
@@ -63,7 +67,7 @@ const Story = () => {
             </div>
             {/* bottom */}
             <div className="pt-5">
-              <h1 className="">JOSEFINE</h1>
+              <h1 className="text-black">JOSEFINE</h1>
               <div className="mt-2">
                 <img src={sign} alt="" className="" />
               </div>
