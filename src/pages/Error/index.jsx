c0fbdeb4be497736639dcb707acd1a22ff798cba
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-09-09 23:52:07 +0600

    error page update

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -6,7 +6,7 @@ const ErrorPage = () => {
     <div className="">
       <Header />
       <h1 className="text-5xl font-extrabold text-slate-900 py-10 text-center">
-        Error Page. Page not found.
+        404 Error Page. Page not found.
       </h1>{" "}
     </div>
   );
