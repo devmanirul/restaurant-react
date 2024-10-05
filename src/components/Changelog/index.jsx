753629d2ea9af91e1b7af147ff1e919472850f03
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-10-05 08:20:33 +0600

    modify protected page

--- src/components/Changelog/index.jsx
+++ src/components/Changelog/index.jsx
@@ -1,11 +1,11 @@
 const Change = () => {
   return (
-    <div className=" flex flex-col items-center justify-center gap-5 py-20">
-      <div className="bg-lightCream py-20 px-40 border border-cream text-center">
+    <div className=" flex flex-col items-center justify-center gap-5 py-14 md:py-40 px-5 md:px-0">
+      <div className="bg-lightCream py-14 md:py-28 px-0 md:px-40 border border-cream text-center">
         <div className="">
-          <h1 className="font-bold text-3xl font-Cormorant border-b border-b-cream inline">
+          <h3 className="text-black text-3xl font-bold font-Cormorant  border-y border-cream inline leading-10">
             Version 1.0
-          </h1>
+          </h3>
         </div>
         <p className="font-josefin mt-3">
           Initial Restaurantate Webflow Template has been released!
--- src/components/ProtectedPage/index.jsx
+++ src/components/ProtectedPage/index.jsx
@@ -1,30 +1,39 @@
+import { Link } from "react-router-dom";
 import Input from "../Shared/Input";
 
 const ProtectedPage = () => {
   return (
     <div className=" flex flex-col items-center justify-center gap-5 py-20">
-      <div className="bg-lightCream py-20 px-40 border border-cream text-center">
+      <div className="bg-lightCream py-24 px-20 border border-cream text-center">
         <div className="flex flex-col justify-center items-center gap-5">
-          <div className="">
+          <div className="bg-black rounded-full p-8">
             <img
               src={"https://i.ibb.co.com/d0JhHFW/Icon.png"}
               alt=""
-              className="bg-black rounded-full p-7"
+              className=""
             />
           </div>
           <h1 className="font-bold text-3xl font-Cormorant border-b border-b-cream inline">
             Protected Page
           </h1>
         </div>
-        <p className="font-josefin mt-3">
+        <p className="font-josefin py-4">
           This page is password protected. If you are the website admin, or have
-          access to this page, please type your password below.{" "}
+          access <br /> to this page, please type your password below.{" "}
         </p>
-        <Input
-          className={"text-black"}
-          type={"password"}
-          placeholder={"Enter Your Password"}
-        />
+        <div className="flex flex-col items-center justify-between gap-5">
+          <input
+            className={"text-black md:py-5 px-8 md:px-9 outline-none border border-black"}
+            type={"password"}
+            placeholder={"Enter Your Password"}
+          />
+          <Link
+            to={"/reservation"}
+            className="bg-cream text-black text-xl font-semibold p-3 md:py-5 px-8 md:px-24"
+          >
+            Submit
+          </Link>
+        </div>
       </div>
     </div>
   );
