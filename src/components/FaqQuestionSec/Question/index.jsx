a65458e76734e2f11b272a42110ac1338fe2f198
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-10-06 20:42:33 +0600

    modify qustion component

--- src/components/FaqQuestionSec/Question/index.jsx
+++ src/components/FaqQuestionSec/Question/index.jsx
@@ -1,19 +1,14 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
 
-const Question = ({ id, question, answer }) => {
-  const [showMore, setShowMore] = useState(false);
+const Question = ({ id, question, answer, handleShowMore, showMore }) => {
   return (
     <button
-      onClick={() => setShowMore(!showMore)}
-      className="text-start py-7 px-10 border border-[#c4c4c4]"
+      onClick={() => handleShowMore(id)}
+      className="text-left py-7 px-10 border border-[#c4c4c4]"
     >
       <h3 className="text-2xl font-medium text-black">{question}</h3>
-
-      {showMore && (
-        <p className="hidden text-xl font-josefin text-lightBlack mt-3">
-          {answer}
-        </p>
+      {showMore === id && (
+        <p className="text-xl font-josefin text-lightBlack mt-3">{answer}</p>
       )}
     </button>
   );
--- src/components/FaqQuestionSec/index.jsx
+++ src/components/FaqQuestionSec/index.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import { faqData } from "../constant/faq.constant";
 import SectionHeading from "../Shared/SectionHeading";
 import Question from "./Question";
 
 const FaqQuestionSec = () => {
+  const [showMore, setShowMore] = useState(null);
+
+  const handleShowMore = (id) => {
+    setShowMore(id);
+  };
   return (
     <div className="container mx-auto my-28">
       <SectionHeading
@@ -13,7 +19,7 @@ const FaqQuestionSec = () => {
       {/* Question container */}
       <div className="max-w-[930px] mx-auto mt-8 flex flex-col gap-4">
         {faqData.map((faq) => (
-          <Question key={faq.id} {...faq} />
+          <Question key={faq.id} {...faq} handleShowMore={handleShowMore} showMore={showMore} />
         ))}
       </div>
     </div>
