03faa3290d8ad1c31415a64d029b5581c8bd8996
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-10-06 23:44:26 +0600

    modify question component

--- src/components/FaqQuestionSec/Question/index.jsx
+++ src/components/FaqQuestionSec/Question/index.jsx
@@ -1,14 +1,24 @@
 /* eslint-disable react/prop-types */
 
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+
 const Question = ({ id, question, answer, handleShowMore, showMore }) => {
   return (
     <button
       onClick={() => handleShowMore(id)}
       className="text-left py-7 px-10 border border-[#c4c4c4]"
     >
-      <h3 className="text-2xl font-medium text-black">{question}</h3>
+      <div className="flex flex-row justify-between items-center gap-5">
+        <h3 className="text-2xl font-medium text-black">{question}</h3>
+        <IoIosArrowDown className="h-10 w-10" />
+      </div>
+
       {showMore === id && (
-        <p className="text-xl font-josefin text-lightBlack mt-3">{answer}</p>
+        <div className="flex flex-row justify-between items-center gap-5">
+          <p className="text-xl font-josefin text-lightBlack mt-3">{answer}</p>
+
+          <IoIosArrowUp className="h-10 w-10" />
+        </div>
       )}
     </button>
   );
