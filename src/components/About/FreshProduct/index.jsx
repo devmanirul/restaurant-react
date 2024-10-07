6b74a320416ed9c842f651cb91ff2eb6f281fdd2
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-10-08 00:00:12 +0600

    gallay update

--- src/components/About/FreshProduct/index.jsx
+++ src/components/About/FreshProduct/index.jsx
@@ -2,20 +2,20 @@ import FreshCard from "./FreshCard";
 import { freshData } from "../constant/about.constant";
 
 const FreshProduct = () => {
-    return (
-      <div className={`bg-freshBg-image`}>
-        <div className="flex flex-col md:flex-row justify-center items-center gap-5 container mx-auto text-white md:h-[496px] md:w-[1920px]">
-          {freshData.map((fresh) => (
-            <FreshCard
-              key={fresh.id}
-              img={fresh.img}
-              title={fresh.title}
-              describe={fresh.describe}
-              see={fresh.see}
-            />
-          ))}
-        </div>
+  return (
+    <div className={`bg-freshBg-image`}>
+      <div className="flex flex-col md:flex-row justify-center items-center gap-5 container mx-auto text-white md:h-[496px] md:w-[1920px]">
+        {freshData.map((fresh) => (
+          <FreshCard
+            key={fresh.id}
+            img={fresh.img}
+            title={fresh.title}
+            describe={fresh.describe}
+            see={fresh.see}
+          />
+        ))}
       </div>
-    );
+    </div>
+  );
 };
 export default FreshProduct;
